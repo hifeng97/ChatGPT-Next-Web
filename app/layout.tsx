350f0056a6b7479326ccc822b66c5973072e08f1
ifen huang <hifeng97@foxmail.com>
2023-04-21 18:34:14 +0000

    upd

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "小疯子的ai",
+  description: "",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "小疯子的ai",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
